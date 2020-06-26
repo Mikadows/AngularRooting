3c911b1decdb9262f585f463cc24bc9c1f3531d6
Gwen <gwendou95@hotmail.fr>
2020-06-26 21:21:50 +0200

    Initial Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router: Router,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+
+  goToServersURL(){
+    this.router.navigate(['servers']);
+  }
+
+  goToUsersURL() {
+    this.router.navigate(['users']);
+  }
+
+}
